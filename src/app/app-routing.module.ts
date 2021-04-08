df91a2ad0e7bf8f7fcb9232753597c73a690cda6
Sebastian Esquivel <s.esquivelmancilla@gmail.com>
2021-04-08 12:41:28 -0400

    create file structure for Git guides

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,65 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchComponent } from './components/search/search.component';
+import { DetalleComponent } from './pages/detalle/detalle.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
 
-const routes: Routes = [];
+// PAGINAS DEL DETALLE
+import { DetalleRegistroComponent } from './pages/detalle-registro/detalle-registro.component';
+import { SeguimientofechasComponent } from './pages/seguimientofechas/seguimientofechas.component';
+import { ProcesolicitatorioComponent } from './pages/procesolicitatorio/procesolicitatorio.component';
+import { InformeseleccionComponent } from './pages/informeseleccion/informeseleccion.component';
+import { AdjudicacionComponent } from './pages/adjudicacion/adjudicacion.component';
+import { ImputacionesComponent } from './pages/imputaciones/imputaciones.component';
+import { ModificacionesComponent } from './pages/modificaciones/modificaciones.component';
+import { LiquidacionComponent } from './pages/liquidacion/liquidacion.component';
+
+import { GarantiasComponent } from './pages/garantias/garantias.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { ListcalendarComponent } from './components/listcalendar/listcalendar.component';
+import { PrimerinformeComponent } from './components/primerinforme/primerinforme.component';
+import { SegundoinformeComponent } from './components/segundoinforme/segundoinforme.component';
+import { TercerinformeComponent } from './components/tercerinforme/tercerinforme.component';
+import { TablaejecucionComponent } from './components/tablaejecucion/tablaejecucion.component';
+import { infoModificacionComponent } from './pages/infoModificacion/infoModificacion.component';
+
+
+
+const routes: Routes = [
+  {path: 'heroes', component: HeroesComponent},
+  //CALENDARIO
+  {path: 'calendar', component: CalendarComponent},
+  {path: 'listcalendar', component: ListcalendarComponent},
+  //INFORMES
+  {path: 'primerinforme', component: PrimerinformeComponent},
+  {path: 'segundoinforme', component: SegundoinformeComponent},
+  {path: 'tercerinforme', component: TercerinformeComponent},
+  //
+  {path: 'tablaejecucion', component: TablaejecucionComponent},
+  
+  {path: 'search/:termino', component: SearchComponent},
+  {path: 'heroe/:id', component: HeroeComponent},  
+  {path: 'detalle/:id', 
+          component: DetalleComponent,
+          children: [
+            {path: 'registro', component: DetalleRegistroComponent},
+            {path: 'fechas', component: SeguimientofechasComponent},
+            {path: 'licitatorio', component: ProcesolicitatorioComponent},
+            {path: 'informe', component: InformeseleccionComponent},
+            {path: 'adjudicacion', component: AdjudicacionComponent},
+            {path: 'garantias', component: GarantiasComponent},
+            {path: 'imputaciones', component: ImputacionesComponent},
+            {path: 'modificaciones', component: ModificacionesComponent,
+            children: [
+                {path: 'info', component: infoModificacionComponent}
+                      ]},
+            {path: 'liquidacion', component: LiquidacionComponent},
+          ]
+        },
+  {path: '**', pathMatch:'full', redirectTo:'heroes'},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,113 @@
+import { FullCalendarModule } from '@fullcalendar/angular';
+import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin
+import interactionPlugin from '@fullcalendar/interaction'; // a plugin
+import listPlugin from '@fullcalendar/list';
+FullCalendarModule.registerPlugins([ // register FullCalendar plugins
+  dayGridPlugin,
+  interactionPlugin,
+  listPlugin
+]);
+
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// SERVICES
+import { HttpClientModule } from '@angular/common/http';
+
+//PAGES
+
+
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearchComponent } from './components/search/search.component';
+import { LoadingComponent } from './components/loading/loading.component';
+import { DetalleComponent } from './pages/detalle/detalle.component';
+import { FilterpipePipe } from './pipes/filterpipe.pipe';
+
+import { DetalleRegistroComponent } from './pages/detalle-registro/detalle-registro.component';
+import { SeguimientofechasComponent } from './pages/seguimientofechas/seguimientofechas.component';
+import { ProcesolicitatorioComponent } from './pages/procesolicitatorio/procesolicitatorio.component';
+import { InformeseleccionComponent } from './pages/informeseleccion/informeseleccion.component';
+import { ImputacionesComponent } from './pages/imputaciones/imputaciones.component';
+import { ContraloriaComponent } from './pages/contraloria/contraloria.component';
+import { ModificacionesComponent } from './pages/modificaciones/modificaciones.component';
+import { LiquidacionComponent } from './pages/liquidacion/liquidacion.component';
+
+//Gráficas
+import { PrimerinformeComponent } from './components/primerinforme/primerinforme.component';
+import { SegundoinformeComponent } from './components/segundoinforme/segundoinforme.component';
+import { TercerinformeComponent } from './components/tercerinforme/tercerinforme.component';
+
+// MatModule
+import { MatTabsModule } from "@angular/material/tabs";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AdjudicacionComponent } from './pages/adjudicacion/adjudicacion.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { GarantiasComponent } from './pages/garantias/garantias.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { ListcalendarComponent } from './components/listcalendar/listcalendar.component';
+
+//NG CHARTS
+import { ChartsModule } from 'ng2-charts';
+import { TablaejecucionComponent } from './components/tablaejecucion/tablaejecucion.component';
+import { infoModificacionComponent } from './pages/infoModificacion/infoModificacion.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroeComponent,
+    HeroesComponent,
+    NavbarComponent,
+    SearchComponent,
+    LoadingComponent,
+    DetalleComponent,
+    FilterpipePipe,
+    infoModificacionComponent,
+    DetalleRegistroComponent,
+    SeguimientofechasComponent,
+    ProcesolicitatorioComponent,
+    InformeseleccionComponent,
+    ImputacionesComponent,
+    ContraloriaComponent,
+    ModificacionesComponent,
+    LiquidacionComponent,    
+    AdjudicacionComponent,
+    HeaderComponent,
+    FooterComponent,
+    GarantiasComponent,
+    CalendarComponent,
+    ListcalendarComponent,
+    PrimerinformeComponent,
+    SegundoinformeComponent,
+    TercerinformeComponent,
+    TablaejecucionComponent,
+    
+
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MatTabsModule,
+    //SERVICE HTTP MODULE
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FullCalendarModule,
+    ChartsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
